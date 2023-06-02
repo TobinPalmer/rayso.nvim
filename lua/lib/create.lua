9bbcbbe2dc5a00f95c2bc23f52a0b4b7af635ca7
TobinPalmer <tobinkpalmer@gmail.com>
2023-06-01 20:15:32 -0700

    split rayso.lua up into modules

---
+++ lua/lib/create.lua
@@ -0,0 +1,65 @@
+local M = {}
+local param_util = require 'lib.params'
+local rayso = require 'rayso'
+
+-- Gets the open command from the config
+M.get_open_command = function()
+  -- On a mac
+  if vim.fn.has 'macunix' then
+    return 'open -a ' .. rayso.config.open_cmd .. '.app'
+  end
+
+  -- Not an mac and command is not an executable
+  if vim.fn.executable(rayso.config.open_cmd) == 0 then
+    return error('Could not find executable for ' .. rayso.config.open_cmd)
+  end
+
+  return rayso.config.open_cmd
+end
+
+-- Creates the snippet
+---@param opts table
+M.create_snippet = function(opts)
+  ---@type open_cmd | nil
+  local open_cmd = M.get_open_command()
+  ---@type string
+  local url
+  ---@type string
+  local query_params
+  ---@type string
+  local code
+
+  if open_cmd == nil then
+    return
+  end
+
+  if opts.args ~= '' then
+    query_params = param_util.generate_query_params()
+    url = rayso.config.base_url .. '' .. opts.args .. '' .. query_params
+
+    -- Get the whole files text as a string
+    --- get the current buffer
+    local whole_file = table.concat(vim.api.nvim_buf_get_lines(0, 0, -1, false), '\n')
+    code = whole_file
+  else
+    local range = vim.api.nvim_buf_get_lines(0, opts.line1 - 1, opts.line2, false)
+    local lines = table.concat(range, '\n', 1, #range)
+    query_params = param_util.generate_query_params(lines)
+    url = param_util.generate_query_params(lines)
+    code = lines
+  end
+  require('lib.file').log(url, code, vim.bo.filetype)
+
+  ---@type string
+  local cmd = open_cmd .. ' ' .. "'" .. url .. "'"
+  vim.fn.system(cmd)
+end
+
+--- Creates the commands for the plugin
+M.create_commands = function()
+  ---@param opts table
+  vim.api.nvim_create_user_command('Rayso', function(opts)
+    require('lib.create').create_snippet(opts)
+  end, { range = '%', nargs = '?' })
+end
+return M
---
+++ lua/lib/file.lua
@@ -0,0 +1,94 @@
+local M = {}
+local rayso = require 'rayso'
+---@param file string
+---@return boolean
+M.file_exists = function(file)
+  local f = io.open(file, 'rb')
+  if f then
+    f:close()
+  end
+  return f ~= nil
+end
+
+---@param file string
+M.lines_from = function(file)
+  if not M.file_exists(file) then
+    return {}
+  end
+  local lines = {}
+  for line in io.lines(file) do
+    lines[#lines + 1] = line
+  end
+  return lines
+end
+
+---@param name string
+---@param path string
+M.create_file = function(path, name)
+  local file = path .. name .. '.md'
+
+  if M.file_exists(file) then
+    return error 'File already exists'
+  end
+
+  -- Create directory if it doesn't exist
+  local success, err = os.execute('mkdir -p "' .. path .. '"')
+  if not success then
+    return error('Failed to create directory: ' .. err)
+  end
+
+  local f, error = io.open(file, 'w')
+  if f then
+    f:close()
+  else
+    if error then
+      return error('Failed to create file:', error)
+    end
+    return "Something went very wrong, couldn't create the file"
+  end
+end
+
+---@param url string
+---@param code string
+---@param lang string
+M.log = function(url, code, lang)
+  if rayso.config.options.logging_enabled == false then
+    return
+  end
+
+  local file = rayso.config.options.logging_path .. rayso.config.options.logging_file .. '.md'
+  if M.file_exists(file) then
+    local f = io.open(file, 'a')
+    --- Check if the code is longer than 5 lines, then add ...
+    if code:match '\n' then
+      local lines = {}
+      for line in code:gmatch '[^\r\n]+' do
+        table.insert(lines, line)
+      end
+      if #lines > 5 then
+        code = ''
+        for i = 1, 5 do
+          code = code .. lines[i] .. '\n'
+        end
+        code = code .. '...'
+      end
+    end
+
+    local code_block = string.format('```%s\n%s\n```\n[link](%s)\n\n', lang, code, url)
+
+    if f == nil then
+      return
+    end
+    local file_, _ = f:write(code_block)
+    if file_ then
+      f:close()
+    else
+      return
+    end
+  else
+    vim.notify("Config file doesn't exist, creating it", vim.log.levels.INFO)
+    M.create_file(M.config.options.logging_path, M.config.options.logging_file)
+  end
+end
+
+return M
